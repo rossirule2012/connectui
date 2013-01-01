fddeaad6f058d39141874f96898e68bbc8ee590e
rossirule2012 <rossirule2012@gmail.com>
2013-01-01 21:22:23 +0100

    Added the support for user-inserted apns

--- connectui.java
+++ connectui.java
@@ -3,6 +3,7 @@ import java.io.*;
 import javax.swing.*;
 import java.awt.event.*;
 import java.lang.Thread.*;
+import java.util.*;
 public class connectui extends JFrame
 {
 	JLabel status = new JLabel("Avviato");
@@ -13,8 +14,8 @@ public class connectui extends JFrame
 	JPanel pan = new JPanel();
 	BorderLayout br = new BorderLayout();
 	FlowLayout fl = new FlowLayout();
-	String[] options = {"internet.wind","internet.tim"}; //here you can add apns EG: {"APN","APN2",...,"APN3"}
-	JList apn = new JList(options);
+	JList apn =null;
+	ArrayList<String> apn_list = new ArrayList<String>();
 	SimpleRunner r = new SimpleRunner();
 	Thread statuses = new Thread(r);
 	
@@ -24,11 +25,11 @@ public class connectui extends JFrame
 		pan.setLayout(fl);
 		pan.add(con);
 		pan.add(dis);
-		add(apn,br.CENTER);
 		add(pan,br.NORTH);
 		add(status,br.SOUTH);
 		actions();
 		statuses.start();
+		listalo();
 		pack();
 		show();
 	} 
@@ -76,8 +77,27 @@ public class connectui extends JFrame
 		runn=false;
 	}
 	
-	
-	
+	public ArrayList<String> apns_enum() throws IOException //It opens  a file called apns_list located into the directory of the .java file. That file contains
+	{														// a list of apns that will be passed to the JList. It's simple for you adding others apns.
+		File apns_file = new File("apns_list.txt");
+		BufferedReader reader = null;
+		ArrayList<String> tmp = new ArrayList<String>();
+		if (!apns_file.exists()) {status.setText("apns file not found, create one please"); System.exit(0);}
+		try {
+				FileInputStream in = new FileInputStream(apns_file);
+				reader= new BufferedReader(new InputStreamReader(in));
+				String tmps = null;
+				while((tmps = reader.readLine()) !=null) {tmp.add(tmps);}
+			}
+		finally {reader.close();}
+		return tmp;
+	}
+	void listalo()
+	{
+		try{apn_list=apns_enum();} catch (IOException errore) {System.out.print("Errore");}
+		JList apn = new JList(apn_list.toArray());
+		add(apn,br.CENTER);
+	}
 public static void main(String[] arg)
 {
 	new connectui();
@@ -87,23 +107,15 @@ class SimpleRunner implements Runnable //this is a background-thread that run al
 {                                      // thanks to the infinite cycle for(;;)
 	public void run()
 	{
-		
 		Process stacontrol;
 		for (;;){
-		
-		
 		try{stacontrol = Runtime.getRuntime().exec("sakis3gz status");
-				BufferedReader in = new BufferedReader(new InputStreamReader(stacontrol.getInputStream()));
-				
-					
+				BufferedReader in = new BufferedReader(new InputStreamReader(stacontrol.getInputStream()));				
 						String returned = in.readLine();
-						
 						if (runn)
 						{} else {status.setText(returned);}
-					
 			}
 		catch(Exception err) {}
-		
 	}}
 }
 }
